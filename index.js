e5a5b9ebd642fecafec910e56ec64aa0eca7fa6b
isabelfuentes7 <84352377+isabelfuentes7@users.noreply.github.com>
2021-08-16 10:55:39 -0400

    updating

---
+++ index.js
@@ -0,0 +1,64 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//function that creates array for user
+
+function promptUser() {
+    return inquirer.prompt([  
+    ])
+}{
+    type: "input",
+    message: "What is the your email?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What is your project name?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What is your project description?",
+    name: "description"
+},
+{
+    type: "input",
+    message: "Who contributed on this project?",
+    name: "contribution"
+},
+
+    
+}
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const init = () => { 
+    promptUser()
+    .then((answers) => writeFileAsync('testREADME.md', generateMarkdown(answers)))
+    .then(() => console.log('Success'))
+    .catch((err) => console.error(err));
+};
+
+// Function call to initialize app
+
+init();
+//async function init(){
+   // try {
+      // const response = await promptUser();
+        //const readMe = geneerateMarkdown(response);
+
+        //await writeFileAsync( "README.md", readMe);
+        //console.log("Success!");
+        //catch (err){
+            //console.log(err);
+
+        }
+    }
+
+    //function call to initialize
+    init();
+
+}
